1079b04a04e649e9dcd4055f0df25449aaf35d80
Hamza <muhammadhamzakhalid24@gmail.com>
2022-10-07 17:03:40 +0500

    Add project files.

---
+++ LearningPhase1/Controllers/RoleController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LearningPhase1.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace LearningPhase1.Controllers
+{
+    public class RoleController : Controller
+    {
+
+        ApplicationDbContext context;
+
+
+        public RoleController()
+        {
+            context = new ApplicationDbContext();
+        }
+
+
+
+
+        // GET: Role
+        public ActionResult Index()
+        {
+            var Roles = context.Roles.ToList();
+            return View(Roles);
+        }
+
+        public ActionResult Create()
+        {
+            var Role = new IdentityRole();
+            return View(Role);
+        }
+
+        [HttpPost]
+        public ActionResult Create(IdentityRole Role)
+        {
+            context.Roles.Add(Role);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
+    }
+}
---
+++ LearningPhase1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LearningPhase1.Startup))]
+namespace LearningPhase1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
